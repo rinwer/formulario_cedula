c78edb050e3a003beed18a1a4d652809e5ddd2a2
rinsonjawer <rinsonjawer@gmail.com>
2026-08-04 10:29:16 -0500

    Se agrega ruta. vercel.jeson

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ ALLOWED_ORIGINS = os.environ.get("ALLOWED_ORIGINS", "http://localhost:5173").spl
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=ALLOWED_ORIGINS
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
